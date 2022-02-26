3810fdf8a3a30ca28faaaee2c4fc956f5246f044
octari <ycjariel@gmail.com>
2022-02-25 23:59:33 -0500

    A5 tuiter fixed who to follow card

--- public/tuiter/WhoToFollowList/index.js
+++ public/tuiter/WhoToFollowList/index.js
@@ -4,6 +4,9 @@ const WhoToFollowList = () => {
     return (`
             <ul class="list-group pseudo-card">
             <!-- continue here -->
+            <div>
+                <p class="profile-summary-follow">Who to follow</>
+            </div>
             ${
                 who.map(whoItem => {
                     return(WhoToFollowListItem(whoItem));
