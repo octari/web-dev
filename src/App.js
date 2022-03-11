eac9a69928b90915bbd228908263fcc8ae40db83
octari <ycjariel@gmail.com>
2022-03-11 18:39:50 -0500

    a6 lab done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import React from 'react';
+
+import './vendors/bootstrap/css/bootstrap.css';
+// import './vendors/bootstrap/bootstrap.min.css';
+import './vendors/fontawesome/css/all.css';
+
+import HelloWorld from "./components/hello-world";
+import Labs from "./components/labs/index";
+import Index from "./components/tuiter/index";
+
+
+
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <div className="container">
+              <Routes>
+                  <Route path="/hello"
+                         exact={true}
+                         element={<HelloWorld/>}/>
+                  <Route path="/"
+                         exact={true}
+                         element={<Labs/>}/>
+                  <Route path="/tuiter"
+                         exact={true}
+                         element={<Index/>}/>
+              </Routes>
+          </div>
+      </BrowserRouter>
+
+
   );
 }
 
