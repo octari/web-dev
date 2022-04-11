93fceb9cb168796f7181096de823815f902e54c1
octari <ycjariel@gmail.com>
2022-04-11 12:58:58 -0400

    a9

--- src/components/tuiter/services/tuit-service.js
+++ src/components/tuiter/services/tuit-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// const API_BASE = process.env.REACT_APP_API_BASE;
+// const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:4000/' ;
 const TUITS_API = `https://full-stack-developer-server.herokuapp.com/api/tuits`;
 
 export const createTuit = async (tuit) => {
